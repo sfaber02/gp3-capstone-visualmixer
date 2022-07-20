d6e61d16b92c63946fab6c846c5298183bfa69fc
JoshMarte <joshuelm@gmail.com>
2022-07-19 21:06:05 -0400

    schudler function

--- backend/util/dailyRotation.js
+++ backend/util/dailyRotation.js
@@ -1,3 +1,22 @@
 // DEPENDENCIES
 require("dotenv").config();
 ACCESS_TOKEN = process.env.DEEZER_TOKEN;
+PLAYLIST = process.env.PLAYLIST;
+
+// TIGGER THIS FUNCTION ????
+
+// GET PLAYLIST DATA
+const getPlaylistData = async () => {
+    let response = await fetch(`${PLAYLIST}?access_token=${ACCESS_TOKEN}`);
+    let apiData = response.json();
+};
+
+// GET DB AUDIO DATA
+
+// COMPARE AND FIND A TRACK NOT IN DB
+
+// DEEZER API --> DELETE AUDIO FROM PLAYLIST
+
+// ADD TRACK TO DB -> GRAB MOST RECENT ()
+
+// UPDATE TRACK API CALL / DB
