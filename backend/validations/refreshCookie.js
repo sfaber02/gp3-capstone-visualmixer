269f61323b19419a7d1f0ef84d0aee95ae3e5aac
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-08-26 18:58:30 -0400

    changed cookie middleware to be triggered by next() and also send the final result

--- backend/validations/refreshCookie.js
+++ backend/validations/refreshCookie.js
@@ -1,13 +1,18 @@
-function refreshCookie(tokens) {
-    return (req, res, next) => {
-        res.cookie("refresh_token", tokens.refreshToken, {
-            httpOnly: true,
-            sameSite: "none",
-            secure: true,
-            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        });
-        next();
-    };
+function refreshCookie(req, res, next) {
+    console.log("refresh cookie");
+    // console.log(res.locals.tokens);
+    let tokens = res.locals.tokens;
+    console.log(tokens.refreshToken);
+
+    res.cookie("refresh_token", tokens.refreshToken, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    });
+
+    res.status(200).json(tokens);
+    
 }
 
 module.exports = refreshCookie;
