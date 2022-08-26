442e1799db083b43a3348bcb6f221400c587ea9c
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-08-26 19:02:33 -0400

    added try/catch to cookie middleware

--- backend/validations/refreshCookie.js
+++ backend/validations/refreshCookie.js
@@ -1,17 +1,23 @@
 function refreshCookie(req, res, next) {
     console.log("refresh cookie");
     // console.log(res.locals.tokens);
-    let tokens = res.locals.tokens;
-    console.log(tokens.refreshToken);
-
-    res.cookie("refresh_token", tokens.refreshToken, {
-        httpOnly: true,
-        sameSite: "none",
-        secure: true,
-        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    });
-
-    res.status(200).json(tokens);
+    
+    try {
+        let tokens = res.locals.tokens;
+        console.log(tokens.refreshToken);
+    
+        res.cookie("refresh_token", tokens.refreshToken, {
+            httpOnly: true,
+            sameSite: "none",
+            secure: true,
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        });
+    
+        res.status(200).json(tokens);
+        
+    } catch (err) {
+         res.status(401).json({ error: error.message });
+    }
     
 }
 
